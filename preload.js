20afbf03fbfe18c7b7a5aba0430a887be8de33db
KorbsStudio <ks@korbsstudio.com>
2021-10-03 05:28:03 -0400

    Add new functions

--- preload.js
+++ preload.js
@@ -1,4 +1,4 @@
-const {contextBridge, ipcRenderer} = require("electron");
+const {contextBridge, ipcMain, ipcRenderer} = require("electron");
 
 contextBridge.exposeInMainWorld(
   "api", {
@@ -7,6 +7,8 @@ contextBridge.exposeInMainWorld(
                                "maximize",
                                "restore",
                                "close",
+                               "blurToggleOn",
+                               "blurToggleOff",
                                "openIn"]; // "openIn" not required, nor are the other options unless you want to keep the custom titlebar
           if (validChannels.includes(channel)) {
               ipcRenderer.send(channel, data);
